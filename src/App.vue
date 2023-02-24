ffc2a45abb91da3225a9c1c19b7156b1e9edcf1a
Marvin Volkmann <marvin.volkmann@adesso.de>
2023-02-24 11:17:06 +0100

    feat: moving cards

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,60 @@
 <script>
+import Card from './components/CardComponent.vue'
+export default {
+  components: {
+    Card: Card
+  },
+  data() {
+    return {
+      currentCard: [],
+      topValueCards: -50,
+    }
+  },
+  methods: {
+    handleMouseDown(e) {
+      if (e.target.classList[0] === "card") {
+        this.currentCard = e.target;
+        window.addEventListener("pointermove", this.moveCard);
+      }
+    },
+    handleMouseUp() {
+      window.removeEventListener("pointermove", this.moveCard);
+    },
+    moveCard(event) {
+      const { clientX, clientY } = event;
+
+      this.currentCard.animate(
+        {
+          left: `${clientX}px`,
+          top: `${clientY}px`,
+        },
+        { duration: 1500, fill: "forwards" }
+      );
+    },
+    moveCards() {
+      this.topValueCards = 50;
+    }
+  }
+}
 </script>
 
 <template>
+  <button @click="moveCards">Click me</button>
+  <div class="card_container" @mousedown="handleMouseDown" @mouseup="handleMouseUp">
+    <Card msg="Hello" left="35" :top="topValueCards"/>
+    <Card msg="Test" left="45" :top="topValueCards"/>
+    <Card msg="Moin" left="55" :top="topValueCards"/>
+    <Card msg="Hilfe" left="65" :top="topValueCards"/>
+  </div>
 </template>
 
 <style>
+body {
+  background-color: black;
+}
+.card_container {
+  position: fixed;
+  width: 100%;
+  height: 100%;
+}
 </style>
---
+++ src/components/CardComponent.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  props: {
+    msg: {
+      type: String,
+      required: true
+    },
+    left: {
+      type: Number,
+      required: true,
+    },
+    top: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      randomDegree: 0,
+    }
+  },
+  beforeMount() {
+    this.randomDegree = this.getRandomInt(-2, 7);
+  },
+  methods: {
+    getRandomInt(min, max) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+  }
+}
+</script>
+
+
+<template>
+  <div class="card" :style="{ 'transform': 'rotate(' + randomDegree + 'deg)', 'left': left + '%', 'top': top + '%'}">{{ msg }}</div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  background-color: white;
+  font-size: 2rem;
+  border-radius: 0.75rem;
+  width: 288px;
+  height: 384px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  user-select: none;
+  translate: -50% -50%;
+  transition: top 1.5s;
+  
+  filter: drop-shadow(0 0 0.75rem black);
+}
+</style>
